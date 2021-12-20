0a98b98d2ce6b483ef50dc81027566e38370b682
Dave Carr <davecarr1024@gmail.com>
2021-12-20 16:55:05 -0500

    position

--- position.py
+++ position.py
@@ -2,7 +2,7 @@ import dataclasses
 import typing
 
 
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass(frozen=True, repr=False)
 class Position:
     @dataclasses.dataclass(frozen=True)
     class Rank:
@@ -15,11 +15,6 @@ class Position:
 
             def __neg__(self) -> 'Position.Rank.Delta':
                 return Position.Rank.Delta(-self.value)
-
-            @staticmethod
-            def test():
-                assert Position.Rank.Delta(-1).value == -1
-                assert -Position.Rank.Delta(-1) == Position.Rank.Delta(1)
 
         value: str
 
@@ -38,33 +33,82 @@ class Position:
         def __sub__(self, delta: Delta) -> 'Position.Rank':
             return self + -delta
 
-        def enumerate(self, delta: Delta) -> typing.List['Position.Rank']:
-            pass
+        def enumerate(self, delta: Delta) -> typing.Generator['Position.Rank', None, None]:
+            val = self
+            while val.can_add(delta):
+                val += delta
+                yield val
 
-        @staticmethod
-        def test():
-            assert Position.Rank('a').can_add(Position.Rank.Delta(0))
-            assert Position.Rank('a').can_add(Position.Rank.Delta(1))
-            assert not Position.Rank('a').can_add(Position.Rank.Delta(-1))
-            assert Position.Rank('d') + Position.Rank.Delta(
-                1) == Position.Rank('e')
-            assert Position.Rank('d') + Position.Rank.Delta(
-                0) == Position.Rank('d')
-            assert Position.Rank('d') + Position.Rank.Delta(
-                -1) == Position.Rank('c')
+    @dataclasses.dataclass(frozen=True)
+    class File:
+        @dataclasses.dataclass(frozen=True)
+        class Delta:
+            value: int
 
-    file: str
-    rank: int
+            def __post_init__(self):
+                assert self.value in (-1, 0, 1)
 
-    def __post__init__(self):
-        assert self.file in 'abcdefgh'
-        assert 1 <= self.rank <= 8
+            def __neg__(self) -> 'Position.File.Delta':
+                return Position.File.Delta(-self.value)
+
+        value: int
+
+        def __post_init__(self):
+            assert 1 <= self.value <= 8
+
+        def can_add(self, delta: Delta) -> bool:
+            return delta.value == 0 or (delta.value == -1 and self.value > 1
+                                        ) or (delta.value == 1
+                                              and self.value < 8)
+
+        def __add__(self, delta: Delta) -> 'Position.File':
+            assert self.can_add(delta)
+            return Position.File(self.value + delta.value)
+
+        def __sub__(self, delta: Delta) -> 'Position.File':
+            return self + -delta
+
+        def enumerate(self, delta: Delta) -> typing.Generator['Position.File', None, None]:
+            val = self
+            while val.can_add(delta):
+                val += delta
+                yield val
+
+    @dataclasses.dataclass(frozen=True)
+    class Delta:
+        drank: 'Position.Rank.Delta'
+        dfile: 'Position.File.Delta'
+
+        def __neg__(self) -> 'Position.Delta':
+            return Position.Delta(-self.drank, -self.dfile)
+
+    rank: Rank
+    file: File
+
+    def __repr__(self) -> str:
+        return f'{self.rank.value}{self.file.value}'
 
     @staticmethod
     def parse(s: str) -> 'Position':
-        return Position(s[0], int(s[1]))
+        assert len(s) == 2
+        return Position(Position.Rank(s[0]), Position.File(int(s[1])))
 
     @staticmethod
-    def test():
-        Position.Rank.test()
-        assert Position.parse('a1') == Position('a', 1)
+    def delta(drank: int, dfile: int) -> Delta:
+        return Position.Delta(Position.Rank.Delta(drank), Position.File.Delta(dfile))
+
+    def can_add(self, delta: Delta) -> bool:
+        return self.rank.can_add(delta.drank) and self.file.can_add(delta.dfile)
+
+    def __add__(self, delta: Delta) -> 'Position':
+        assert self.can_add(delta)
+        return Position(self.rank + delta.drank, self.file + delta.dfile)
+
+    def __sub__(self, delta: Delta) -> 'Position':
+        return self + -delta
+
+    def enumerate(self, delta: Delta) -> typing.Generator['Position', None, None]:
+        val = self
+        while val.can_add(delta):
+            val += delta
+            yield val
--- position_test.py
+++ position_test.py
@@ -1,8 +1,149 @@
+from position import Position
 import unittest
+import itertools
 
-class PositionTest:
-    def test_rank_delta_value(self):
-        self.assertEqual(Position.Rank.Delta(-1).value, -1)
+
+class PositionRankDeltaTest(unittest.TestCase):
+    def test_ctor(self):
+        for val in (-1, 0, 1):
+            with self.subTest(val=val):
+                self.assertEqual(Position.Rank.Delta(val).value, val)
+
+    def test_ctor_fail(self):
+        with self.assertRaises(AssertionError):
+            Position.Rank.Delta(2)
+
+    def test_neg(self):
+        self.assertEqual(-Position.Rank.Delta(1),
+                         Position.Rank.Delta(-1))
+
+
+class PositionRankTest(unittest.TestCase):
+    def test_ctor_fail(self):
+        with self.assertRaises(AssertionError):
+            Position.Rank('i')
+
+    def test_add(self):
+        self.assertEqual(Position.Rank(
+            'a') + Position.Rank.Delta(1), Position.Rank('b'))
+
+    def test_add_fail(self):
+        with self.assertRaises(AssertionError):
+            Position.Rank('h') + Position.Rank.Delta(1)
+
+    def test_sub(self):
+        self.assertEqual(Position.Rank(
+            'c') - Position.Rank.Delta(1), Position.Rank('b'))
+
+    def test_enumerate_zero(self):
+        self.assertListEqual(list(itertools.islice(Position.Rank('a').enumerate(
+            Position.Rank.Delta(0)), 3)), [Position.Rank('a')]*3)
+
+    def test_enumerate_right(self):
+        self.assertListEqual(list(Position.Rank('f').enumerate(Position.Rank.Delta(1))),
+                             [
+                                 Position.Rank('g'),
+                                 Position.Rank('h'),
+        ]
+        )
+
+    def test_enumerate_left(self):
+        self.assertListEqual(list(Position.Rank('c').enumerate(Position.Rank.Delta(-1))),
+                             [
+                                 Position.Rank('b'),
+                                 Position.Rank('a'),
+        ]
+        )
+
+
+class PositionFileDeltaTest(unittest.TestCase):
+    def test_ctor(self):
+        for val in (-1, 0, 1):
+            with self.subTest(val=val):
+                self.assertEqual(Position.File.Delta(val).value, val)
+
+    def test_ctor_fail(self):
+        with self.assertRaises(AssertionError):
+            Position.File.Delta(2)
+
+    def test_neg(self):
+        self.assertEqual(-Position.File.Delta(1),
+                         Position.File.Delta(-1))
+
+
+class PositionFileTest(unittest.TestCase):
+    def test_ctor_fail(self):
+        with self.assertRaises(AssertionError):
+            Position.File(9)
+
+    def test_add(self):
+        self.assertEqual(Position.File(
+            1) + Position.File.Delta(1), Position.File(2))
+
+    def test_add_fail(self):
+        with self.assertRaises(AssertionError):
+            Position.File(8) + Position.File.Delta(1)
+
+    def test_sub(self):
+        self.assertEqual(Position.File(
+            3) - Position.File.Delta(1), Position.File(2))
+
+    def test_enumerate_zero(self):
+        self.assertListEqual(list(itertools.islice(Position.File(1).enumerate(
+            Position.File.Delta(0)), 3)), [Position.File(1)]*3)
+
+    def test_enumerate_right(self):
+        self.assertListEqual(list(Position.File(6).enumerate(Position.File.Delta(1))),
+                             [
+                                 Position.File(7),
+                                 Position.File(8),
+        ]
+        )
+
+    def test_enumerate_left(self):
+        self.assertListEqual(list(Position.File(3).enumerate(Position.File.Delta(-1))),
+                             [
+                                 Position.File(2),
+                                 Position.File(1),
+        ]
+        )
+
+    def test_enumerate_left_empty(self):
+        self.assertListEqual(list(Position.File(
+            1).enumerate(Position.File.Delta(-1))), [])
+
+
+class PositionDeltaTest(unittest.TestCase):
+    def test_neg(self):
+        self.assertEqual(-Position.Delta(Position.Rank.Delta(1), Position.File.Delta(-1)),
+                         Position.Delta(Position.Rank.Delta(-1), Position.File.Delta(1)))
+
+
+class PositionTest(unittest.TestCase):
+    def test_parse(self):
+        self.assertEqual(Position.parse('c2'), Position(
+            Position.Rank('c'), Position.File(2)))
+
+    def test_delta(self):
+        self.assertEqual(Position.delta(1, 0), Position.Delta(
+            Position.Rank.Delta(1), Position.File.Delta(0)))
+
+    def test_can_add(self):
+        self.assertTrue(Position.parse('d8').can_add(Position.delta(1, 0)))
+        self.assertFalse(Position.parse('d8').can_add(Position.delta(0, 1)))
+
+    def test_add(self):
+        self.assertEqual(Position.parse('d4') +
+                         Position.delta(1, -1), Position.parse('e3'))
+
+    def test_sub(self):
+        self.assertEqual(Position.parse('d4') -
+                         Position.delta(1, -1), Position.parse('c5'))
+
+    def test_enumerate(self):
+        self.assertListEqual(list(Position.parse('f4').enumerate(Position.delta(1, -1))),
+                             [Position.parse('g3'), Position.parse('h2')])
+
 
 if __name__ == '__main__':
     unittest.main()
